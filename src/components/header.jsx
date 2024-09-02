5908812d49e7bd6d57dc5792dfe68d375e4344b9
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-09-02 17:09:17 -0500

    feat: :tada: Starting portfolio structure

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <nav className="flex flex-col justify-between w-[70vw]">
+            <a href="">Inicio</a>
+            <a href="">Acerca de mi</a>
+            <a href="">Projects</a>
+            <a href="">Habilidades</a>
+            <a href="">Educacion</a>
+        </nav>
+    )
+}
