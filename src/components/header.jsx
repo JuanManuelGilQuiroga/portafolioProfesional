9852de6b5735ffe2c7972ff72f0cdfb78b867828
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-09-03 11:12:18 -0500

    feat: :sparkles: Portfolio almost completed, open to changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ export function Header() {
         <nav className="flex flex-row items-center justify-between w-[50vw] h-[15vh]">
             <a href="#inicio">Inicio</a>
             <a href="#acercaDeMi">Acerca de mi</a>
-            <a href="#proyectos">Projects</a>
+            <a href="#proyectos">Proyectos</a>
             <a href="#habilidades">Habilidades</a>
             <a href="#objetivos">Valores y Objetivos</a>
         </nav>
