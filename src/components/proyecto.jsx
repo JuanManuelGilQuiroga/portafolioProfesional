5bc71eb3ebb734aa78441036033f09cb8b5382e4
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-10-17 17:33:34 -0500

    refactor: :lipstick: Changes in the styles for the animation of the project card at the time of doing hover

--- src/components/proyecto.jsx
+++ src/components/proyecto.jsx
@@ -1,11 +1,17 @@
 import { motion } from 'framer-motion'
+import { useState } from 'react';
+import { CodeIcon } from '../assets/codeIcon'
 
 export function Proyecto ({titulo, imagen, descripcion, tecnologias, link, isInView, posicion}) {
+    const [isHovered, setIsHovered] = useState(false);
 
-    console.log(posicion)
     return (
         <motion.div animate={{opacity: isInView ? 1 : 0}} initial={{opacity: 0}} transition={{duration: .5, delay: posicion*.5}} className="flex flex-col gap-2 h-[60vh] w-[40%]">
-            <img src={imagen} alt="" className="h-[50%] object-contain"/>
+            <motion.div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='h-[50%] relative'>
+                <motion.img style={{ filter: isHovered ? "brightness(0.5)" : "brightness(1)" }} transition={{ duration: 0.3 }} src={imagen} alt="" className="h-full object-cover"/>
+                <motion.a href={link} style={{ opacity: isHovered ? 1 : 0 }}
+                transition={{ duration: 1 }} className='w-[10%] h-[18%] bg-custom-cream absolute bottom-4 left-4 rounded-xl flex justify-center items-center'><CodeIcon style="h-[50%] text-custom-black"/></motion.a>
+            </motion.div>
             <div className="flex flex-col justify-between">
                 <h3 className="text-[1.2rem] font-semibold">{titulo}</h3>
                 <p className="font-medium">{descripcion}</p>
